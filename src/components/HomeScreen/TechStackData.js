8759c096b1ea612a9288d4baf21f45435fb92503
Rajesh Yadav <rajeshyadav1@Rajeshs-MacBook-Air.local>
2024-04-29 15:08:52 +0530

    Completed The Section 3 part

---
+++ src/components/HomeScreen/TechStackData.js
@@ -0,0 +1,53 @@
+import { TechStackImages } from "../../constants/Images"
+
+export const data = [
+    {
+        id : 1,
+        logo : TechStackImages.frontendLogo,
+        name : "Front End",
+        content : "Elevate your digital presence with our frontend expertise. Seamlessly blend design and functionality to create captivating user experiences that leave a lasting impression.",
+        backgroundImage : TechStackImages.frontendBg,
+    },
+    {
+        id : 2,
+        logo : TechStackImages.dataBaseLogo,
+        name : "Database",
+        content : "Unlock the power of data with our expertly crafted databases. Seamlessly manage and analyze information to drive your business forward with precision and ease.",
+        backgroundImage : TechStackImages.dataBaseBg,
+    },
+    {
+        id : 3,
+        logo : TechStackImages.cloudLogo,
+        name : "Cloud",
+        content : "Explore the digital sky's limitless possibilities. With our cloud solutions, you can take your company to new heights where creativity and boundless possibilities collide, turning concepts into reality.",
+        backgroundImage : TechStackImages.cloudBg,
+    },
+    {
+        id : 4,
+        logo : TechStackImages.designLogo,
+        name : "Design",
+        content : "With compelling design solutions, elevate your brand. Our designs, which are crafted to inspire and innovate, redefine excellence and distinguish your company in every way.",
+        backgroundImage : TechStackImages.designBg,
+    },
+    {
+        id : 5,
+        logo : TechStackImages.backendLogo,
+        name : "Back End",
+        content : "Crafting the digital canvas, our backend design weaves elegance and efficiency. Where code meets creativity, we architect the foundation for seamless user experiences.",
+        backgroundImage : TechStackImages.backendBg,
+    },
+    {
+        id : 6,
+        logo : TechStackImages.deveOpsLogo,
+        name : "DeveOps",
+        content : "DevOps at our core, we craft designs to soar. Seamlessly integrate, innovate, and adore, for every challenge, we're ready to explore.",
+        backgroundImage : TechStackImages.deveOpsLogo,
+    },
+    {
+        id : 7,
+        logo : TechStackImages.analyticsLogo,
+        name : "Analytics",
+        content : "Unlock insights, drive decisions. Harness the power of data with our analytics solutions. Transforming information into actionable strategies for your success.",
+        backgroundImage : TechStackImages.analyticsBg,
+    },
+]
---
+++ src/constants/Images.js
@@ -0,0 +1,25 @@
+export const TechStackImages = {
+    frontendLogo : require("../assets/techStack/frontend.png"),
+    frontendBg : require("../assets/techStack/frontendBG.jpg"),
+
+    dataBaseLogo : require("../assets/techStack/database.png"),
+    dataBaseBg : require("../assets/techStack/databaseBG.jpg"),
+
+    cloudLogo : require("../assets/techStack/cloud.png"),
+    cloudBg : require("../assets/techStack/cloudBG.jpg"),
+
+    designLogo : require("../assets/techStack/Design.png"),
+    designBg : require("../assets/techStack/designBG.jpg"),
+
+    backendLogo : require("../assets/techStack/setting.png"),
+    backendBg : require("../assets/techStack/backendBG.jpg"),
+
+    deveOpsLogo : require("../assets/techStack/infinity.png"),
+    deveOpsBg : require("../assets/techStack/designBG.jpg"),
+
+    analyticsLogo : require("../assets/techStack/analytic.png"),
+    analyticsBg : require("../assets/techStack/analyticsBG.jpg"),
+
+    plusIcon : require("../assets/techStack/plusicon.png"),
+    minusIcon : require("../assets/techStack/minusicon.png"),
+}
