97581252ae095b244774c37076c41f988fb4b30e
Rajesh <ry292000@gmail.com>
2024-05-01 19:01:23 +0530

    resolved the deployed the issue

--- src/constants/Images.js
+++ src/constants/Images.js
@@ -1,96 +1,95 @@
 export const TechStackImages = {
-    frontendLogo : require("../assets/techStack/frontend.png"),
-    frontendBg : require("../assets/techStack/frontendBG.jpg"),
+  frontendLogo: require("../assets/techStack/frontend.png"),
+  frontendBg: require("../assets/techStack/frontendBG.jpg"),
 
-    dataBaseLogo : require("../assets/techStack/database.png"),
-    dataBaseBg : require("../assets/techStack/databaseBG.jpg"),
+  dataBaseLogo: require("../assets/techStack/database.png"),
+  dataBaseBg: require("../assets/techStack/databaseBG.jpg"),
 
-    cloudLogo : require("../assets/techStack/cloud.png"),
-    cloudBg : require("../assets/techStack/cloudBG.jpg"),
+  cloudLogo: require("../assets/techStack/cloud.png"),
+  cloudBg: require("../assets/techStack/cloudBG.jpg"),
 
-    designLogo : require("../assets/techStack/Design.png"),
-    designBg : require("../assets/techStack/designBG.jpg"),
+  designLogo: require("../assets/techStack/Design.png"),
+  designBg: require("../assets/techStack/designBG.jpg"),
 
-    backendLogo : require("../assets/techStack/setting.png"),
-    backendBg : require("../assets/techStack/backendBG.jpg"),
+  backendLogo: require("../assets/techStack/setting.png"),
+  backendBg: require("../assets/techStack/backendBG.jpg"),
 
-    deveOpsLogo : require("../assets/techStack/infinity.png"),
-    deveOpsBg : require("../assets/techStack/designBG.jpg"),
+  deveOpsLogo: require("../assets/techStack/infinity.png"),
+  deveOpsBg: require("../assets/techStack/designBG.jpg"),
 
-    analyticsLogo : require("../assets/techStack/analytic.png"),
-    analyticsBg : require("../assets/techStack/analyticsBG.jpg"),
+  analyticsLogo: require("../assets/techStack/analytic.png"),
+  analyticsBg: require("../assets/techStack/analyticsBG.jpg"),
 
-    plusIcon : require("../assets/techStack/plusicon.png"),
-    minusIcon : require("../assets/techStack/minusicon.png"),
-}
+  plusIcon: require("../assets/techStack/plusicon.png"),
+  minusIcon: require("../assets/techStack/minusicon.png"),
+};
 
 export const OurApproach = {
-    Ideate : require("../assets/OurApproach/Ideateicon.png"),
-    Ideate1 : require("../assets/OurApproach/1.png"),
+  Ideate: require("../assets/OurApproach/Ideateicon.png"),
+  Ideate1: require("../assets/OurApproach/1.png"),
 
-    Design : require("../assets/OurApproach/Designicon.png"),
-    Design2 : require("../assets/OurApproach/2.png"),
+  Design: require("../assets/OurApproach/Designicon.png"),
+  Design2: require("../assets/OurApproach/2.png"),
 
-    Develop : require("../assets/OurApproach/Developicon.png"),
-    Develop3 : require("../assets/OurApproach/3.png"),
+  Develop: require("../assets/OurApproach/Developicon.png"),
+  Develop3: require("../assets/OurApproach/3.png"),
 
-    Deploy : require("../assets/OurApproach/Deployicon.png"),
-    Deploy4 : require("../assets/OurApproach/4.png"),
-    
-}
+  Deploy: require("../assets/OurApproach/Deployicon.png"),
+  Deploy4: require("../assets/OurApproach/4.png"),
+};
 
 export const clientLogo = {
-    apexon : require("../assets/ClientLogo/apexon.png"),
+  apexon: require("../assets/ClientLogo/apexon.png"),
 
-    aurotech : require("../assets/ClientLogo/aurotech.png"),
+  aurotech: require("../assets/ClientLogo/aurotech.png"),
 
-    enrich : require("../assets/ClientLogo/enrich.png"),
+  enrich: require("../assets/ClientLogo/enrich.png"),
 
-    etaillinsights : require("../assets/ClientLogo/etailinsights1.png"),
+  etaillinsights: require("../assets/ClientLogo/etailinsights1.png"),
 
-    fci : require("../assets/ClientLogo/fci.png"),
+  fci: require("../assets/ClientLogo/fci.png"),
 
-    ispace : require("../assets/ClientLogo/ispace.png"),
+  ispace: require("../assets/ClientLogo/ispace.png"),
 
-    itpeople : require("../assets/ClientLogo/itpeople.png"),
+  itpeople: require("../assets/ClientLogo/itpeople.png"),
 
-    lenovo : require("../assets/ClientLogo/lenovo.png"),
+  lenovo: require("../assets/ClientLogo/lenovo.png"),
 
-    mds : require("../assets/ClientLogo/MDS.png"),
+  mds: require("../assets/ClientLogo/MDS.png"),
 
-    myxx : require("../assets/ClientLogo/myxx.png"),
+  myxx: require("../assets/ClientLogo/myxx.png"),
 
-    noval : require("../assets/ClientLogo/Novelcx.png"),
+  noval: require("../assets/ClientLogo/Novelcx.png"),
 
-    nxivi : require("../assets/ClientLogo/nxivi.png"),
+  nxivi: require("../assets/ClientLogo/nxivi.png"),
 
-    openGate : require("../assets/ClientLogo/openGateCapital.png"),
+  openGate: require("../assets/ClientLogo/openGateCapital.png"),
 
-    slate : require("../assets/ClientLogo/salate.png"),
+  slate: require("../assets/ClientLogo/salate.png"),
 
-    weShape : require("../assets/ClientLogo/weShape.png"),
+  weShape: require("../assets/ClientLogo/weShape.png"),
 
-    wunderkind : require("../assets/ClientLogo/wunderkind.png"),
-}
+  wunderkind: require("../assets/ClientLogo/wunderkind.png"),
+};
 
 export const ourClientSay = {
-    Erik : require("../assets/OurClientsSays/ErikMinges.png"),
-    Frances : require("../assets/OurClientsSays/FrancesHo.png"),
-    Marc : require("../assets/OurClientsSays/Marc.png"),
-    Quincy : require("../assets/OurClientsSays/Quincy.png"),
-    Tobias : require("../assets/OurClientsSays/Tobias.png"),
-}
+  Erik: require("../assets/OurClientsSays/ErikMinges.png"),
+  Frances: require("../assets/OurClientsSays/FrancesHo.png"),
+  Marc: require("../assets/OurClientsSays/Marc.png"),
+  // Quincy : require("../assets/OurClientsSays/Quincy.png"),
+  Tobias: require("../assets/OurClientsSays/Tobias.png"),
+};
 
 export const ourOffice = {
-    map : require("../assets/OurOffice/Map.png"),
-    location : require("../assets/OurOffice/Location.png"),
-    indiaLoction : require("../assets/OurOffice/IndiaLocation.png"),
-    USLocation : require("../assets/OurOffice/USLocation.png"),
-}
+  map: require("../assets/OurOffice/Map.png"),
+  location: require("../assets/OurOffice/Location.png"),
+  indiaLoction: require("../assets/OurOffice/IndiaLocation.png"),
+  USLocation: require("../assets/OurOffice/USLocation.png"),
+};
 
 export const social = {
-    youtube : require("../assets/SocialImage/YoutubeLogo.png"),
-    twitter : require("../assets/SocialImage/TwitterLogo.png"),
-    linkdin : require("../assets/SocialImage/LinkdinLogo.png"),
-    instagram : require("../assets/SocialImage/InstagramLogo.png"),
-}
+  youtube: require("../assets/SocialImage/YoutubeLogo.png"),
+  twitter: require("../assets/SocialImage/TwitterLogo.png"),
+  linkdin: require("../assets/SocialImage/LinkdinLogo.png"),
+  instagram: require("../assets/SocialImage/InstagramLogo.png"),
+};
